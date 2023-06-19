fd3c48688ca7d3f3dfb47338e7da0950ed0fd54b
IlyaZolotuhin <1zolotyhin1@gmail.com>
2023-06-19 19:10:22 +0700

    uncontrolled 19.06 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import './App.css';
-import Accordion from "./components/Accordion/Accordion";
+//import Accordion from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
 import {OnOff} from "./components/OnOff/OnOff";
+import UnControlledAccordion from "./components/UnControlledAccordion/UnControlledAccordion";
+import {UnControlledRating} from "./components/UnControlledRating/UnControlledRating";
+
 
 function App() {
     console.log('App rendering')
     return (
-        <div>
+        <div className={"App"}>
             <PageTitle title={"This is App component"}/>
             <PageTitle title={"My friends"}/>
             Article 1
             <Rating value={3}/>
-            <Accordion titleValue={"Menu"} collapsed={true}/>
-            <Accordion titleValue={"Users"} collapsed={false}/>
+            <UnControlledAccordion titleValue={"Menu"} />
+            <UnControlledAccordion titleValue={"Users"} />
             Article 2
-            <Rating value={0}/>
-            <Rating value={1}/>
+            <UnControlledRating />
+            {/*<Rating value={1}/>
             <Rating value={2}/>
             <Rating value={3}/>
             <Rating value={4}/>
-            <Rating value={5}/>
+            <Rating value={5}/>*/}
             Article 2
             <OnOff />
             {/*<OnOff turnOn={true}/>*/}
---
+++ src/components/UnControlledAccordion/UnControlledAccordion.tsx
@@ -0,0 +1,48 @@
+import React, {useState} from "react";
+
+type AccordionPropsType = {
+    titleValue: string
+    //collapsed: boolean
+}
+
+function UnControlledAccordion(props: AccordionPropsType) {
+    console.log('Accordion rendering')
+
+    let [collapsed,setCollapsed] = useState(false);
+
+    const onclickToggle = () => {
+        setCollapsed(!collapsed)
+    }
+
+              return (
+            <div>
+                <AccordionTitle title={props.titleValue}/><button onClick={onclickToggle}>TOGGLE</button>
+                {!collapsed && <AccordionBody/>}
+            </div>
+        );
+
+}
+
+type  AccordionTitlePropsType = {
+    title: string
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    console.log('AccordionTitle rendering')
+    return (
+        <h3>{props.title}</h3>
+    );
+}
+
+function AccordionBody() {
+    console.log('AccordionBody rendering')
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    );
+}
+
+export default UnControlledAccordion;
